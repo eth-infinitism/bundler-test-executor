33bf1a6b004606f45b4f9f8a5df58e02ec2c3a60
Dror Tirosh <dror@opengsn.org>
2023-01-28 03:09:29 +0200

    push results to separate repo

---
+++ update-results.sh
@@ -0,0 +1,21 @@
+#!/bin/bash -xe
+
+if [ -z "$1" ]; 
+then
+echo usage: $0 {results-clone-folder}
+exit 1
+fi
+
+base=`pwd`/$1
+rm -rf $base
+git clone --depth 1 https://github.com/eth-infinitism/bundler-test-results.git $base
+ts=`date +%Y%m%d/%H%M%S`
+folder=$base/runs/$ts
+mkdir -p $folder
+cp build/out/* $folder
+ls -p $folder > $folder/index.txt
+cd $base/runs
+ln -nsf $ts latest
+ls -p > index.txt
+find . > all.txt
+
