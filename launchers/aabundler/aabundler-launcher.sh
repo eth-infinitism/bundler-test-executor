15b3e134cb31dc25581d1e1beadb28d493cbf485
Dror Tirosh <dror@opengsn.org>
2023-02-23 21:35:11 +0200

    debug wait for start

--- launchers/aabundler/aabundler-launcher.sh
+++ launchers/aabundler/aabundler-launcher.sh
@@ -7,12 +7,14 @@ cd `dirname \`realpath $0\``
 case $1 in
 
  name)
-	echo "AA Reference Bundler/$TAG"
+	echo "AA-Reference-Bundler/$TAG"
 	;;
 
  start)
 	docker-compose up -d
-	while ! [[  `curl 2>/dev/null  -X POST http://localhost:3000/rpc` =~ error ]]; do sleep 1 ; done
+	echo waiting for bundler to start
+	#while ! [[  `curl 2>/dev/null  -X POST http://localhost:3000/rpc` =~ error ]]; do sleep 1 ; done
+	while ! [[  `curl -X POST http://localhost:3000/rpc` =~ error ]]; do echo waiting for bundler; sleep 4 ; done
 	;;
  stop)
  	docker-compose down
