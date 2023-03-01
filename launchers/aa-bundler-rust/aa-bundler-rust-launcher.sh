3555af2a2b6d7568a5d5688d4edbb5f46d93ac04
Vid Kersic <vid.kersic@yahoo.com>
2023-03-01 08:11:41 -0700

    feat: add aa-bundler Rust

---
+++ launchers/aa-bundler-rust/aa-bundler-rust-launcher.sh
@@ -0,0 +1,21 @@
+#!/bin/bash 
+# Launcher script for the aa-bundler in Rust.
+
+cd `dirname \`realpath $0\``
+case $1 in
+
+ name)
+	echo "aa-bundler in Rust"
+	;;
+
+ start)
+	docker-compose up -d
+	cd ../../bundler-spec-tests/@account-abstraction && yarn deploy --network localhost
+	;;
+ stop)
+ 	docker-compose down
+	;;
+
+ *)
+	echo "usage: $0 {start|stop|name}"
+esac
