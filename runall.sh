2b6a5184024b51e2cf116ba5c93553d162799ef7
Dror Tirosh <dror@opengsn.org>
2023-03-14 20:53:59 +0200

    runall changes
    - allow passing launcher script on cmdline
      use ./runall.sh ./launcher/xxx [options]
      to run just that launcher
      (options passed down to pytest, e.g. "-k GAS")
    
    - junit (xml and json) now contains stdout of failed tests.

--- runall.sh
+++ runall.sh
@@ -1,9 +1,15 @@
-#!/bin/bash -xe
+#!/bin/bash -e
 root=`cd \`dirname $0\`; pwd`
 
 BUILD=$root/build
 OUT=$BUILD/out
 test -d bundler-spec-tests || git clone https://github.com/eth-infinitism/bundler-spec-tests.git
+
+LAUNCHERS="`pwd`/launchers/*.sh"
+if [ -n "$1" -a -r "$1" ]; then
+LAUNCHERS=`pwd`/$1
+shift
+fi
 
 cd bundler-spec-tests 
 
@@ -19,7 +25,8 @@ fi
 rm -rf $OUT
 mkdir -p $OUT
 
-for launcher in ../launchers/*.sh; do
+for launcher in $LAUNCHERS; do 
+
 #skip folders
 test -d $launcher && continue
 
@@ -34,7 +41,9 @@ outraw=$OUT/$basename.txt
 
 name=`$launcher name`
 echo "Running launcher $launcher, name=$name" > $outraw
-pdm run test --launcher-script=$launcher -o junit_suite_name="$name"  --junit-xml $outxml "$@" | tee -a $outraw
+OPTIONS="--launcher-script=$launcher --junit-xml $outxml"
+OPTIONS="$OPTIONS -o junit_logging=all -o junit_log_passing_tests=false --log-rpc"
+pdm run test -o junit_suite_name="$name" $OPTIONS "$@" | tee -a $outraw
 xq . $outxml > $outjson
 
 cat $outjson
