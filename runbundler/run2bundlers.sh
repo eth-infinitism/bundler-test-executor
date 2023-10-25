2491d2e0e243f6e003d30a0d0b3e35c2cff5b055
Dror Tirosh <dror.tirosh@gmail.com>
2023-10-25 13:02:04 +0300

    run 2 bundlers script
    launcher script to start 2 bundlers.
    "bundler" is the boot-node, and "bundler2" is a peer node, so it is
    delayed until the bootnode is up.

---
+++ runbundler/run2bundlers.sh
@@ -0,0 +1,32 @@
+#!/bin/bash -xe
+dir=`dirname $0`
+
+if [ -z "$2" ] ; then
+       echo usage: "$0 {bootyml} {start|stop|..}"
+       exit 1 
+fi
+
+DC="docker-compose --env-file $dir/run2bundlers.env -f $dir/run2bundlers.yml"
+
+export BUNDLER_YML=`realpath $1`
+export BUNDLER2_YML=`realpath $2`
+ENVFILE=`dirname $BUNDLER_YML`/.env
+if [ -r $ENVFILE ] ; then
+	export `grep -v '#' $ENVFILE`
+fi
+#env for bundler2 ?
+
+cmd=$3
+shift
+shift
+shift
+case "$cmd" in 
+
+	start) $DC run --rm wait-for-bundler ;;
+        down) $DC down -t 1 ;;
+	stop) $DC stop -t 1 ;;
+	#execute misc docker-compose command
+	*) $DC $cmd $* ;;
+
+esac
+
