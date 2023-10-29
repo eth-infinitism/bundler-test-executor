58d6b13fe99928b5ea44af6bf3fe2619246fb541
Dror Tirosh <dror.tirosh@gmail.com>
2023-10-29 20:30:07 +0200

    launch through standard launcher runall
    - each p2p test is an "env" file under the tests folder.
    - test file define the 1st and 2nd bundler to use, and possibly more env
    params.
    
    to run the test:
     P2P=1 ./runall.sh tests/skandha-skandha.env -k p2p
    
    skandha-skandha.env - test file, specifying boot and peer nodes.
    
    P2P=1 - the test is skipped otherwise (added so that normal tests won't
    attempt to launch p2p tests)
    -k p2p - launch only p2p tests in this run (thourhg it is possible to
    run all others too, on the first (boot) bundler)

--- runbundler/runbundler.sh
+++ runbundler/runbundler.sh
@@ -1,25 +1,57 @@
-#!/bin/bash
+#!/bin/bash  -x
 dir=`dirname $0`
 
-if [ -z "$2" ] ; then
-       echo usage: "$0 {ymlfile} {start|stop|..}"
-       exit 1 
-fi
+function usage() {
+cat <<EOF
+usage: $0 {ymlfile|testfile} {start|stop|..}
+  ymlfile -  a single bundler yml file. uses DCFILE="runbundler.yml"
+  testfile - env file to define all launch env. params.
+  cmd:
+  	start (wait for wait-for-bundler task to exit)
+  	any docker-compose command (e.g: up,start,down,logs)
+  testfile expected env.vars:
+    DCFILE - the docker-compose.yml file to use. defaults to run2bundlers.yml
+    BUNDLER_YML - bundler-specific file for bootnode bundler
+    BUNDLER2_YML - bundler-specific file for peer bundler
+	ENVFILE, ENVFILE2 - extra env.vars used by above bundlers
+EOF
+exit 1 
 
-DC="docker-compose -f $dir/runbundler.yml"
-export BUNDLER_YML=`realpath $1`
-ENVFILE=`dirname $BUNDLER_YML`/.env
-if [ -r $ENVFILE ] ; then
-	export `grep -v '#' $ENVFILE`
-fi
+}
 
+file=$1
 cmd=$2
 shift
 shift
+
+test -z "$cmd" && usage
+
+case "$file" in 
+	*.yml)
+		export DCFILE="$dir/runbundler.yml"
+		export DCPARAMS="--env-file $dir/runbundler.env"
+		ENVFILE=`dirname $BUNDLER_YML`/.env
+		test -r $ENVFILE && DCPARAMS="$DCPARAMS --env-file $ENVFILE"
+		export BUNDLER_YML=$file
+		;;
+
+	*.env)
+		export DCFILE="$dir/run2bundlers.yml"
+		export DCPARAMS="--env-file $dir/run2bundlers.env"
+		source $file
+		test -r "$ENVFILE" && DCPARAMS="$DCPARAMS --env-file $ENVFILE"
+		test -r "$ENVFILE2" && DCPARAMS="$DCPARAMS --env-file $ENVFILE2"
+		;;
+	*)	usage ;;
+
+esac
+
+DC="docker-compose $DCPARAMS -f $dir/../empty.yml -f $DCFILE"
+cmd=$cmd
 case "$cmd" in 
 
 	start) $DC run --rm wait-for-bundler ;;
-        down) $DC down -t 1 ;;
+	down) $DC down -t 1 ;;
 	stop) $DC stop -t 1 ;;
 	#execute misc docker-compose command
 	*) $DC $cmd $* ;;
