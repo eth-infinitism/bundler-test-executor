e907d14ee9989d31dddf52ace64f3620de73006b
Dror Tirosh <dror.tirosh@gmail.com>
2023-12-01 00:49:25 +0200

    silius boot node, silius-silius config
    start with
    `./runbundler/runbundler.sh ./tests/silius-silius.env up`
    both bundlers start, but dont communicate.

--- runbundler/runbundler.sh
+++ runbundler/runbundler.sh
@@ -30,6 +30,12 @@ function docker-compose1 {
 docker run --rm -v /var/run/docker.sock:/var/run/docker.sock -v $root:$root -v $TMPENV:$TMPENV -w="$PWD" docker:24-cli compose "$@"
 }
 
+function envFileFor {
+	file=$1
+	envfile=`perl -e "\$file=$file;" -e '$file=~s/[.]\w+/.env/; print $file'`;
+	test -r $envfile && cat $envfile
+}
+
 file=`realpath $1`
 cmd=$2
 shift
